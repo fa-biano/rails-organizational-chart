71433771eb6493a5e20b0a96832efd7ce16d2d05
Fa-biano <fbiano.souza@gmail.com>
2025-08-30 01:17:34 -0300

    feat: created React root element and App

---
+++ app/javascript/components/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Organizational Chart</h1>
+      <p>This is a placeholder for the React application.</p>
+    </div>
+  )
+}
+
+export default App
