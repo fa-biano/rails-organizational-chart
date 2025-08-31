e15d8c016f9ee47b89c7e4dcf6801752d3da9843
Fa-biano <fbiano.souza@gmail.com>
2025-08-30 21:36:40 -0300

    test: added coverage to companies controller

---
+++ test/controllers/companies_controller_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class CompaniesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @company = companies(:uol)
+  end
+
+  test "should get index" do
+    get companies_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_company_url
+    assert_response :success
+  end
+
+test "should create company" do
+  assert_difference("Company.count", 1) do
+    post companies_url, params: { company: { name: "New Company" } }, as: :json
+  end
+
+  assert_response :created
+
+  json = JSON.parse(response.body)
+  assert_equal "New Company", json["name"]
+end
+
+  test "should show company" do
+    get company_url(@company)
+    assert_response :success
+  end
+end
