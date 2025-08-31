ebd4c93447ffed53cd3f2e058ac50c8720048eaf
Fa-biano <fbiano.souza@gmail.com>
2025-08-31 19:41:39 -0300

    feat: added rescue for notfound records with method record_not_found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found(error)
+    model = error.model || "Record"
+    render json: { error: "#{model} not found" }, status: :not_found
+  end
 end
