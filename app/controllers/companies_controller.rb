3e3746d8a2c0496195e87b32192c61149c921a03
Fa-biano <fbiano.souza@gmail.com>
2025-08-31 15:53:00 -0300

    feat: adjust companies create and show to return json

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,21 +12,17 @@ class CompaniesController < ApplicationController
   end
 
   def create
-    @company = Company.new(company_params)
-    if @company.save
-      render json: @company, status: :created
+    company = Company.new(company_params)
+    if company.save
+      render json: company, status: :created
     else
-      render json: @company.errors, status: :unprocessable_entity
+      render json: company.errors, status: :unprocessable_entity
     end
   end
 
   def show
-    @company = Company.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @company }
-    end
+    company = Company.find(params[:id])
+    render json: company
   end
 
   def company_params
