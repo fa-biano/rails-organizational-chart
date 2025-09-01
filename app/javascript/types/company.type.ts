6ea1f0fa5848f002769c978711e206325763a90b
Fa-biano <fbiano.souza@gmail.com>
2025-08-31 22:36:18 -0300

    feat: added company and employee interfaces

---
+++ app/javascript/types/company.type.ts
@@ -0,0 +1,9 @@
+import { IEmployee } from './employee.type.'
+
+export interface ICompany {
+  id: string
+  name: string
+  created_at: string
+  updated_at: string
+  employees?: IEmployee[]
+}
---
+++ app/javascript/types/employee.type..ts
@@ -0,0 +1,9 @@
+export interface IEmployee {
+  id: string
+  name: string
+  email: string
+  picture?: string
+  company_id: string
+  created_at: string
+  updated_at: string
+}
